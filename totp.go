9c713923b3ad897400cf0a1bbf6bf4bac36c8243
Del Putnam <del@putnams.net>
2016-05-27 15:34:34 -0400

    Initial commit

---
+++ totp.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/base32"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/hgfischer/go-otp"
+)
+
+var (
+	secret   = flag.String("secret", "AAAABBBBCCCCDDDD", "Secret key")
+	isBase32 = flag.Bool("base32", true, "If true, the secret is interpreted as a Base32 string")
+	length   = flag.Uint("length", otp.DefaultLength, "OTP length")
+	period   = flag.Uint("period", otp.DefaultPeriod, "Period in seconds")
+	counter  = flag.Uint64("counter", 0, "Counter")
+)
+
+func main() {
+	flag.Parse()
+
+	key := *secret
+	if !*isBase32 {
+		key = base32.StdEncoding.EncodeToString([]byte(*secret))
+	}
+
+	key = strings.ToUpper(key)
+
+	totp := &otp.TOTP{
+		Secret:         key,
+		Length:         uint8(*length),
+		Period:         uint8(*period),
+		IsBase32Secret: true,
+	}
+	fmt.Println(totp.Get())
+}
