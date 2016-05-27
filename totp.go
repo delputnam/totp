442330f4b1181cf95442be64ac343f65b67c3382
Del Putnam <del@putnams.net>
2016-05-27 17:18:52 -0400

    Fixed bad string pointer rerference

--- totp.go
+++ totp.go
@@ -4,37 +4,43 @@ import (
 	"encoding/base32"
 	"flag"
 	"fmt"
+	"log"
 	"strings"
 
 	"github.com/hgfischer/go-otp"
 )
 
 var (
-	secret   = flag.String("secret", "", "Secret key")
-	isBase32 = flag.Bool("base32", true, "If true, the secret is interpreted as a Base32 string")
-	length   = flag.Uint("length", otp.DefaultLength, "OTP length")
-	period   = flag.Uint("period", otp.DefaultPeriod, "Period in seconds")
-	counter  = flag.Uint64("counter", 0, "Counter")
+	secret   string
+	isBase32 bool
+	length   uint
+	period   uint
+	counter  uint64
 )
 
 func main() {
+	flag.StringVar(&secret, "secret", "", "Secret key")
+	flag.BoolVar(&isBase32, "base32", true, "If true, the secret is interpreted as a Base32 string")
+	flag.UintVar(&length, "length", otp.DefaultLength, "OTP length")
+	flag.UintVar(&period, "period", otp.DefaultPeriod, "Period in seconds")
+	flag.Uint64Var(&counter, "counter", 0, "Counter")
 	flag.Parse()
 
 	if secret == "" {
 		log.Fatal("Must provide a secret.")
 	}
 
-	key := *secret
-	if !*isBase32 {
-		key = base32.StdEncoding.EncodeToString([]byte(*secret))
+	key := secret
+	if !isBase32 {
+		key = base32.StdEncoding.EncodeToString([]byte(secret))
 	}
 
 	key = strings.ToUpper(key)
 
 	totp := &otp.TOTP{
 		Secret:         key,
-		Length:         uint8(*length),
-		Period:         uint8(*period),
+		Length:         uint8(length),
+		Period:         uint8(period),
 		IsBase32Secret: true,
 	}
 	fmt.Println(totp.Get())
